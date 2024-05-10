5b598b5f6bcd35a3455c9f2dd688f5818fd8d461
Seongil Yoon <seonyoon@c4r2s3.42seoul.kr>
2024-05-10 18:01:57 +0900

    fix crash when no password input

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:59:23 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/05/10 15:06:08 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/05/10 18:01:13 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 #include "Server.hpp"
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
+    if (argc != 2 && argc != 3) {
         std::cerr << "Usage: ./ircserv <port> <passwd>" << std::endl;
         return 1;
     }
     Server server;
-    server.Init(std::atoi(argv[1]), std::string(argv[2]));
+    std::string passwd = (argc == 3) ? std::string(argv[2]) : "";
+    server.Init(std::atoi(argv[1]), passwd);
     server.Listen();
     return 0;
 }
