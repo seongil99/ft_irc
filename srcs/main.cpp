86c464cc81d53d93b151cec7b3fa639ee3ad71c4
Seongil Yoon <seonyoon@c4r2s3.42seoul.kr>
2024-05-10 15:06:40 +0900

    fix main passwd input

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:59:23 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/05/07 14:59:08 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/05/10 15:06:08 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,12 @@
 #include "Server.hpp"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        // todo: add password input => ./ircserv <port> <passwd>
-        std::cerr << "Usage: ./ircserv <port>" << std::endl;
+    if (argc < 2) {
+        std::cerr << "Usage: ./ircserv <port> <passwd>" << std::endl;
         return 1;
     }
     Server server;
-    server.Init(std::atoi(argv[1]));
+    server.Init(std::atoi(argv[1]), std::string(argv[2]));
     server.Listen();
     return 0;
 }
